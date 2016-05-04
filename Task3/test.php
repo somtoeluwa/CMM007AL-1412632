db18688a08165393b53b4bab78ab5f9141ccbc56
somtoeluwa <1412632@rgu.ac.uk>
2016-05-04 12:09:32 +0100

    add.php test

---
+++ Task3/test.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1412632
+ * Date: 04/05/2016
+ * Time: 12:03
+ */
+
+echo $_POST['entryTitle'] ;
+ echo $_POST['submitter'];
+ echo $_POST['category'] ;
+echo $_POST['entrySummary'];
